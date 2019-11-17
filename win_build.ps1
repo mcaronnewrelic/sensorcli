892c4aacf4859cee90fd0f70ced417b40d31219d
michael caron <michael.caron@gmail.com>
2019-11-17 03:16:07 -0800

    adding gitinore

--- win_build.ps1
+++ win_build.ps1
@@ -16,79 +16,79 @@ $targetName = "$target.exe"
 $v = $version.Split(".")
 
 if ($v.Length -ne 3) {
-    echo "-version must follow a numeric major.minor.patch semantic versioning schema (received: $version)"
+    Write-Output "-version must follow a numeric major.minor.patch semantic versioning schema (received: $version)"
     exit -1
 }
 
 $wrong = $v | ? { (-Not [System.Int32]::TryParse($_, [ref]0)) -or ( $_.Length -eq 0) -or ([int]$_ -lt 0)} | % { 1 }
 if ($wrong.Length  -ne 0) {
-    echo "-version major, minor and patch must be valid positive integers (received: $version)"
+    Write-Output "-version major, minor and patch must be valid positive integers (received: $version)"
     exit -1
 }
 
-echo "--- Checking dependencies"
+Write-Output "--- Checking dependencies"
 
-echo "Checking Go..."
+Write-Output "Checking Go..."
 go version
 if (-not $?)
 {
-    echo "Can't find Go"
+    Write-Output "Can't find Go"
     exit -1
 }
 
-echo "--- Checking MSBuild.exe..."
+Write-Output "--- Checking MSBuild.exe..."
 $msBuild = (Get-ItemProperty hklm:\software\Microsoft\MSBuild\ToolsVersions\4.0).MSBuildToolsPath
 if ($msBuild.Length -eq 0) {
-    echo "Can't find MSBuild tool. .NET Framework 4.0.x must be installed"
+    Write-Output "Can't find MSBuild tool. .NET Framework 4.0.x must be installed"
     exit -1
 }
-# echo $msBuild
+# Write-Output $msBuild
 
 $env:GOOS="windows"
 $env:GOARCH=$arch
 
-echo "--- Collecting files"
+Write-Output "--- Collecting files"
 
 $goFiles = go list ./...
 
-echo "--- Check that go-bindata has generated the data"
+Write-Output "--- Check that go-bindata has generated the data"
 $EmbeddedFileName="Data.go"
 if ((Test-Path "src\$EmbeddedFileName" -PathType Leaf) -eq $False) {
     go-bindata -o "./src/$EmbeddedFileName" data/
-    echo "data file was created."
+    Write-Output "data file was created."
 }
 
-echo "--- Format check"
+Write-Output "--- Format check"
 
 $wrongFormat = go fmt $goFiles
 
 if ($wrongFormat -and ($wrongFormat.Length -gt 0))
 {
-    echo "ERROR: Wrong format for files:"
-    echo $wrongFormat
+    Write-Output "ERROR: Wrong format for files:"
+    Write-Output $wrongFormat
     exit -1
 }
 
-echo "--- Running Build"
+Write-Output "--- Running Build"
 
 #go build -v $goFiles
 if (-not $?)
 {
-    echo "Failed building files"
+    Write-Output "Failed building files"
     exit -1
 }
 
-echo "--- Collecting Go main files"
+Write-Output "--- Collecting Go main files"
 
 $packages = go list -f "{{.ImportPath}} {{.Name}}" ./...  | ConvertFrom-String -PropertyNames Path, Name
 $mainPackage = $packages | ? { $_.Name -eq 'main' } | % { $_.Path }
 
-echo "--- Generating $target package"
+Write-Output "--- Generating $target package"
 go generate $mainPackage
 
 $fileName = ([io.fileinfo]$mainPackage).BaseName
 
-echo "--- Creating $targetName"
+Write-Output "--- Creating $targetName"
 go build -ldflags "-X main.buildVersion=$version" -o ".\build\windows_$arch\$targetName" $mainPackage
 
 
